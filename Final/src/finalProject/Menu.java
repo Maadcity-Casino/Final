9b4aee7c2a48423ae0e7575eb01c6db33e9901fb
JackReis01 <jackreis2019@gmail.com>
2017-10-11 09:14:49 -0500

    Updates

---
+++ Final/src/finalProject/Menu.java
@@ -0,0 +1,32 @@
+package finalProject;
+import javax.swing.JOptionPane;
+public class Menu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String user;
+		do {
+		user = JOptionPane.showInputDialog(null,
+				"What game would you like to play?\n1 - Coin flip\n2 - Rock Paper Scissors"
+				+ "\n3 - Guess\n4 - Bingo\n5 - Black Jack\n6 - Quit");
+		if("Coin flip".equalsIgnoreCase(user)) {
+			CoinFlip.main(args);
+	}
+		if("Rock Paper Scissors".equalsIgnoreCase(user)) {
+			RockPaperScissors.main(args);
+		}
+		if("guess".equalsIgnoreCase(user)) {
+			Guess.main(args);
+		}
+		//if("Bingo".equalsIgnoreCase(user)) {
+		//	Bingo.main(args);
+		//}
+		if("Black Jack".equalsIgnoreCase(user)) {
+			BlackJack.main(args);
+		}
+		if("quit".equalsIgnoreCase(user)) {
+			
+		}
+		}while ("Coin flip".equalsIgnoreCase(user)||"Rock paper Scissors".equalsIgnoreCase(user)||"Guess".equalsIgnoreCase(user)||"Bingo".equalsIgnoreCase(user)||"Black Jack".equalsIgnoreCase(user) );
+	}
+}
